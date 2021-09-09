6ba761364d8aeb686020858ffd76577ba2fc63ab
milan.simic <milan.simic@ingkomora.rs>
2021-09-09 18:44:14 +0200

    constants -> osiguranja

--- config/constants.php
+++ config/constants.php
@@ -32,34 +32,46 @@ define("ZAHTEV_LICENCA_OTKLJUCAN", 21);
 define("ZAHTEV_LICENCA_ZAKLJUCAN", 22);
 
 //STATUS LICENCE
-define('LICENCA_AKTIVNA','A'); //20
-define('LICENCA_NEAKTIVNA','N'); //21
-define('LICENCA_DEAKTIVIRANA','D'); //22
-define('LICENCA_NEAKTIVIRANA','H'); //23
+define('LICENCA_AKTIVNA', 'A'); //20
+define('LICENCA_NEAKTIVNA', 'N'); //21
+define('LICENCA_DEAKTIVIRANA', 'D'); //22
+define('LICENCA_NEAKTIVIRANA', 'H'); //23
 
 //STATUS DOKUMENTA
 define('DOKUMENT_ORIGINAL', 24);
 define('DOKUMENT_FOTOKOPIJA', 25);
 define('DOKUMENT_DIGITALNA_KOPIJA', 26);
 
+//ZAHTEVI ZA MIROVANJE
+define('MIROVANJE_ZAHTEV_KREIRAN', 27);
+define('MIROVANJE_ZAHTEV_GENERISAN', 28);
+define('MIROVANJE_ZAHTEV_PRIMLJEN', 29);
+define('MIROVANJE_ZAHTEV_OBRADJEN', 30);
+define('MIROVANJE_ZAHTEV_ODOBREN', 31);
+//MIROVANJE
+define('MIROVANJE_AKTIVNO', 32);
+define('MIROVANJE_ISTEKLO', 33);
+define('MIROVANJE_PREKINUTO', 34);
+
 //UNIVERZALNI ZAHTEVI (SVECANA FORMA, ...)
-define('ZAHTEV_KREIRAN',27);//todo 2
-define('ZAHTEV_GENERISAN',28); //todo 3
-define('ZAHTEV_ZAVRSEN',29); //todo 8
-define('ZAHTEV_ODBIJEN',30);
-define('ZAHTEV_ZASTAREO',31);
+//define('ZAHTEV_KREIRAN', 27);//todo 2
+//define('ZAHTEV_GENERISAN', 28); //todo 3
+//define('ZAHTEV_ZAVRSEN', 29); //todo 8
+//define('ZAHTEV_ODBIJEN', 30);
+//define('ZAHTEV_ZASTAREO', 31);
 
 //ZAPOSLENJE
 //sad je zaposlen 3
 
 //GRUPE STATUSA
-define('OPSTA',1);
-define('STRUCNI_ISPIT',2);
-define('CLANSTVO',3);
-define('LICENCE',4);
-define('REGISTAR',5);
-define('DOKUMENTA',6);
-define('OSIGURANJE',7);
+define('OPSTA', 1);
+define('STRUCNI_ISPIT', 2);
+define('CLANSTVO', 3);
+define('LICENCE', 4);
+define('REGISTAR', 5);
+define('DOKUMENTA', 6);
+define('MIROVANJA', 7);
+define('OSIGURANJE', 8);
 
 define('VRSTA_LICENCE_SVE', 0);
 define('VRSTA_LICENCE_PLANIRANJE', 1);
@@ -69,44 +81,46 @@ define('VRSTA_LICENCE_IZVODJENJE', 4);
 define('VRSTA_LICENCE_PROJEKTOVANJE_IZVODJENJE', 5);
 
 //VRSTA LICENCE
-define('PLANERI',1);
-define('URBANISTI',2);
-define('PROJENTANTI',3);
-define('IZVODJACI',4);
+define('PLANERI', 1);
+define('URBANISTI', 2);
+define('PROJENTANTI', 3);
+define('IZVODJACI', 4);
 
 //POKRICE POLISE
-define('PLANIRANJE',1);
-define('URBANIZAM',2);
-define('PROJEKTOVANJE',3);
-define('IZVODENJE_RADOVA',4);
-define('PROJEKTOVANJE_IZVODENJE',5);
-define('PROJEKTOVANJE_URBANIZAM_PLANIRANJE',6);
-define('PROJEKTOVANJE_IZVODENJE_URBANIZAM_PLANIRANJE',7);
-define('NEMA_POKRICE',99);
+define('PLANIRANJE', 1);
+define('URBANIZAM', 2);
+define('PROJEKTOVANJE', 3);
+define('IZVODENJE_RADOVA', 4);
+define('PROJEKTOVANJE_IZVODENJE', 5);
+define('PROJEKTOVANJE_URBANIZAM_PLANIRANJE', 6);
+define('PROJEKTOVANJE_IZVODENJE_URBANIZAM_PLANIRANJE', 7);
+define('NEMA_POKRICE', 99);
 
-//OSIGURANJE IKS
-define('OSIGURANJE_IKS',1);
+//OSIGURANJE
+define('OSIGURANJE_IKS', 1);
+define('OSIGURANJE_KOLEKTIVNO', 2);
+define('OSIGURANJE_INDIVIDUALNO', 3);
 
 //TODO: VADITI IZ BAZE
 define('ARHITEKTE', array(1, 15, 30, 33, 34, 35));
-define('PEJZ_ARHITEKTE', array(6,58));
+define('PEJZ_ARHITEKTE', array(6, 58));
 
-define('PROFESIONALNI_NAZIV',array(
-    1=>['Odgovorni planer'=>'Odgovornog planera',
-        'Odgovorni urbanista'=>'Odgovornog urbanistu',
-        'Odgovorni projektant'=>'Odgovornog projektanta',
-        'Odgovorni izvođač'=>'Odgovornog izvođača',
-        'Odgovorni izvođač radova'=>'Odgovornog izvođača radova',
-        'Odgovorni inženjer'=>'Odgovornog inženjera'],
-    2=>['Odgovorni prostorni planer'=>'Odgovornog prostornog planera',
-        'Odgovorni urbanista'=>'Odgovornog urbanistu',
-        'Odgovorni projektant'=>'Odgovornog projektanta',
-        'Odgovorni izvođač radova'=>'Odgovornog izvođača radova'],
-    3=>['Prostorni planer'=>'Prostornog planera',
-        'Urbanista'=>'Urbanistu',
-        'Arhitekta urbanista'=>'Arhitektu urbanistu',
-        'Inženjer'=>'Inženjera',
-        'Pejzažni arhitekta'=>'Pejzažnog arhitektu',
-        'Arhitekta'=>'Arhitektu',
-        'Izvođač radova'=>'Izvođača radova']
+define('PROFESIONALNI_NAZIV', array(
+    1 => ['Odgovorni planer' => 'Odgovornog planera',
+        'Odgovorni urbanista' => 'Odgovornog urbanistu',
+        'Odgovorni projektant' => 'Odgovornog projektanta',
+        'Odgovorni izvođač' => 'Odgovornog izvođača',
+        'Odgovorni izvođač radova' => 'Odgovornog izvođača radova',
+        'Odgovorni inženjer' => 'Odgovornog inženjera'],
+    2 => ['Odgovorni prostorni planer' => 'Odgovornog prostornog planera',
+        'Odgovorni urbanista' => 'Odgovornog urbanistu',
+        'Odgovorni projektant' => 'Odgovornog projektanta',
+        'Odgovorni izvođač radova' => 'Odgovornog izvođača radova'],
+    3 => ['Prostorni planer' => 'Prostornog planera',
+        'Urbanista' => 'Urbanistu',
+        'Arhitekta urbanista' => 'Arhitektu urbanistu',
+        'Inženjer' => 'Inženjera',
+        'Pejzažni arhitekta' => 'Pejzažnog arhitektu',
+        'Arhitekta' => 'Arhitektu',
+        'Izvođač radova' => 'Izvođača radova']
 ));
