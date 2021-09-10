ac42d33d045c3ff8eccf548cadbc4fa720d6ecc5
milan.simic <milan.simic@ingkomora.rs>
2021-09-10 08:41:28 +0200

    OsiguranjeRequest iskljucen rule ugovarac_osiguranja_mb

--- app/Http/Requests/OsiguranjeRequest.php
+++ app/Http/Requests/OsiguranjeRequest.php
@@ -35,7 +35,7 @@ class OsiguranjeRequest extends FormRequest
             'polisaPokrice' => 'required', // polisa_pokrice_id
             'FirmaOsiguravajucaKuca' => 'required',
             'osobe' => 'required', // korisnici_osiguranja
-            'ugovarac_osiguranja_mb' => 'required_without:ugovarac_osiguranja_osoba_id|required_unless:osiguranje_tip_id,' . OSIGURANJE_INDIVIDUALNO,
+//            'ugovarac_osiguranja_mb' => 'required_without:ugovarac_osiguranja_osoba_id|required_unless:osiguranje_tip_id,' . OSIGURANJE_INDIVIDUALNO,
 //            'ugovarac_osiguranja_osoba_id' => 'required_without:ugovarac_osiguranja_mb|required_if:osiguranje_tip_id,' . OSIGURANJE_INDIVIDUALNO,
             'osiguranje_tip_id' => 'required',
             'status_polise_id' => 'required',
