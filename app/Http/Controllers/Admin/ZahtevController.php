4ca395b4cc7711c27dd1481f602b92c41bbd09bf
bojan.maravic <bojan.maravic@ingkomora.rs>
2021-09-14 09:57:18 +0200

    ZahtevController.php reformat greska  - =>-

--- app/Http/Controllers/Admin/ZahtevController.php
+++ app/Http/Controllers/Admin/ZahtevController.php
@@ -28,22 +28,19 @@ use Prologue\Alerts\Facades\Alert;
 use Session;
 
 
-class ZahtevController extends Controller
-{
+class ZahtevController extends Controller {
     protected $data = []; // the information we send to the view
     protected $h;
 
     /**
      * Create a new controller instance.
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->h = new Helper();
         $this->middleware(backpack_middleware());
     }
 
-    public function unesi($action, $url = '')
-    {
+    public function unesi($action, $url = '') {
         if (Session::get('message') !== NULL) {
             $data['message'] = Session::get('message');
         }
@@ -61,8 +58,7 @@ class ZahtevController extends Controller
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Illuminate\View\View|\Symfony\Component\HttpFoundation\BinaryFileResponse
      */
-    public function obradizahtevsvecanaforma(Request $request)
-    {
+    public function obradizahtevsvecanaforma(Request $request) {
 
         $file = $request->file('upload');
 
@@ -312,13 +308,11 @@ class ZahtevController extends Controller
 
     }
 
-    public function createZip($zipfile)
-    {
+    public function createZip($zipfile) {
         return $zipfile;
     }
 
-    public function downloadZip(Request $request)
-    {
+    public function downloadZip(Request $request) {
 //        dd($request->zipfile);
         ob_end_clean();
         //TODO: lokacija sa koje se fajl preuzima
@@ -331,8 +325,7 @@ class ZahtevController extends Controller
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function unesinovelicence(Request $request)
-    {
+    public function unesinovelicence(Request $request) {
         $file = $request->file('upload');
         if (!is_null($file)) {
 //            UNOS LICENCI IZ EXCEL DATOTEKE
@@ -498,14 +491,12 @@ class ZahtevController extends Controller
         return redirect('/admin/unesinovelicence')->with('message', $messageLicencaOK)->with('messageNOK', $messageLicencaNOK)->withInput();
     }
 
-    private function checkDate($date)
-    {
+    private function checkDate($date) {
         $dt = DateTime::createFromFormat("Y-m-d", $date);
         return $dt !== false && !array_sum($dt::getLastErrors());
     }
 
-    private function checkOsoba($jmbg)
-    {
+    private function checkOsoba($jmbg) {
         $osoba = Osoba::find($jmbg);
         if (!is_null($osoba)) {
             return true;
@@ -520,8 +511,7 @@ class ZahtevController extends Controller
      * @param $tip
      * @return null
      */
-    private function getJMBG($broj, $tip)
-    {
+    private function getJMBG($broj, $tip) {
         $jmbg = NULL;
         switch ($tip) {
             case 'zahtev':
@@ -546,8 +536,7 @@ class ZahtevController extends Controller
      * @param string $tip
      * @return \stdClass
      */
-    private function getZahtevLicenca($broj, $tip = 'broj_licence')
-    {
+    private function getZahtevLicenca($broj, $tip = 'broj_licence') {
         $response = new \stdClass();
         switch ($tip) {
             case 'broj_zahteva':
@@ -585,8 +574,7 @@ class ZahtevController extends Controller
      * @param $licenca
      * @return \stdClass
      */
-    private function azurirajZahtevLicenca(ZahtevLicenca $zahtev, $licenca)
-    {
+    private function azurirajZahtevLicenca(ZahtevLicenca $zahtev, $licenca) {
         $response = new \stdClass();
         $tipLicence = LicencaTip::find($licenca['tip']);
         if (is_null($tipLicence)) {
@@ -623,8 +611,7 @@ class ZahtevController extends Controller
      * @param $licenca
      * @return \stdClass
      */
-    private function kreirajZahtevLicenca($licenca)
-    {
+    private function kreirajZahtevLicenca($licenca) {
         $response = new \stdClass();
 
         $zahtev = new ZahtevLicenca();
@@ -637,8 +624,7 @@ class ZahtevController extends Controller
      * @param $broj_licence
      * @return \stdClass
      */
-    private function getLicenca($broj_licence)
-    {
+    private function getLicenca($broj_licence) {
         $response = new \stdClass();
         $licenca = Licenca::find($broj_licence);
         if (!is_null($licenca)) {
@@ -662,8 +648,7 @@ class ZahtevController extends Controller
      * @param ZahtevLicenca $zahtev
      * @return \stdClass
      */
-    private function azurirajLicencu(Licenca $licenca, ZahtevLicenca $zahtev)
-    {
+    private function azurirajLicencu(Licenca $licenca, ZahtevLicenca $zahtev) {
         $response = new \stdClass();
         $licenca->id = $zahtev->licenca_broj;
         $licenca->licencatip = $zahtev->licencatip;
@@ -694,8 +679,7 @@ class ZahtevController extends Controller
      * @param ZahtevLicenca $zahtev
      * @return \stdClass
      */
-    private function kreirajLicencu(ZahtevLicenca $zahtev)
-    {
+    private function kreirajLicencu(ZahtevLicenca $zahtev) {
 //dd($zahtev);
         $licenca = new Licenca();
         $response = $this->azurirajLicencu($licenca, $zahtev);
@@ -713,8 +697,7 @@ class ZahtevController extends Controller
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
-    public function unesinoveclanove(Request $request)
-    {
+    public function unesinoveclanove(Request $request) {
         $messageOK = "";
         $messageNOK = "";
         $file = $request->file('upload');
@@ -769,8 +752,7 @@ class ZahtevController extends Controller
     }
 
 
-    public function odobrinovogclana($prijava)
-    {
+    public function odobrinovogclana($prijava) {
         $resp = new \stdClass();
         $prijava_clan = PrijavaClanstvo::find($prijava['broj']);
 
@@ -795,15 +777,15 @@ class ZahtevController extends Controller
             $lib->dodeliJedinstveniLib($osoba->id, Auth::user()->id);
             $this->logOsoba($osoba, CLANSTVO, "Ažurirana osoba: $osoba->ime $osoba->prezime($osoba->id), lib: $osoba->lib, status: $osoba->clan");
 
-            $prijava_clan->datum_prijema = Carbon::parse($prijava['datum - prijema'])->format('Y-m-d');
-            $prijava_clan->zavodni_broj = $prijava['zavodni - broj'];
-            $prijava_clan->datum_odluke_uo = Carbon::parse($prijava['datum - resenja'])->format('Y-m-d');
-            $prijava_clan->broj_odluke_uo = $prijava['broj - resenja'];
+            $prijava_clan->datum_prijema = Carbon::parse($prijava['datum-prijema'])->format('Y-m-d');
+            $prijava_clan->zavodni_broj = $prijava['zavodni-broj'];
+            $prijava_clan->datum_odluke_uo = Carbon::parse($prijava['datum-resenja'])->format('Y-m-d');
+            $prijava_clan->broj_odluke_uo = $prijava['broj-resenja'];
             $prijava_clan->status_id = PRIJAVA_CLAN_PRIHVACENA;
             $prijava_clan->save();
             $this->log($prijava_clan, CLANSTVO, "Kreirana prijava za clanstvo osoba: $osoba->ime $osoba->prezime($osoba->id), status: " . PRIJAVA_CLAN_PRIHVACENA);
             $clanarina = DB::table('tclanarinaod2006')->updateOrInsert(
-                ['osoba' => $osoba->id, 'rokzanaplatu' => Carbon::parse($prijava['datum - resenja'])->format('Y-m-d')],
+                ['osoba' => $osoba->id, 'rokzanaplatu' => Carbon::parse($prijava['datum-resenja'])->format('Y-m-d')],
                 [
                     'iznoszanaplatu' => 7500,
                     'created_at' => now()
@@ -820,16 +802,14 @@ class ZahtevController extends Controller
         return $resp;
     }
 
-    private function getOsobaLicence($osoba)
-    {
+    private function getOsobaLicence($osoba) {
 
     }
 
     /*
      * funkcija koja se poziva iz bladea AJAX
      */
-    public function checkLicencaTip(Request $request)
-    {
+    public function checkLicencaTip(Request $request) {
         $licTip4 = strtoupper(substr(trim($request->input('licence.*.broj')[0]), 0, 4));
         $licencaTip = LicencaTip::where("id", $licTip4)->pluck('naziv', 'id')->toArray();
         if ($licencaTip) {
@@ -848,8 +828,7 @@ class ZahtevController extends Controller
     /*
      * funkcija koja se poziva iz bladea AJAX
      */
-    public function getLicencaTip($id)
-    {
+    public function getLicencaTip($id) {
         $id = substr($id, 0, 3);
         $licencaTip = LicencaTip::where("id", 'LIKE', $id . '%')->get()->pluck('tip_naziv', 'id')->toArray();
         return json_encode($licencaTip);
@@ -859,16 +838,14 @@ class ZahtevController extends Controller
     /*
      * funkcija koja se poziva iz bladea AJAX
      */
-    public function checkZahtev($licenca, $jmbg)
-    {
+    public function checkZahtev($licenca, $jmbg) {
 //        dd($licenca . $jmbg);
 
         return json_encode(true);
     }
 
 //    TODO dodati ovo u helper ili LOG klasu
-    private function log($object, $statusGrupa, $naziv, $napomena = '')
-    {
+    private function log($object, $statusGrupa, $naziv, $napomena = '') {
 //        dd($object->id);
         $log = Log::firstOrNew(['naziv' => $naziv, 'loggable_id' => $object->id]);
         $log->naziv = $naziv;
@@ -878,8 +855,7 @@ class ZahtevController extends Controller
         $log->save();
     }
 
-    private function logOsoba($object, $statusGrupa, $naziv, $napomena = '')
-    {
+    private function logOsoba($object, $statusGrupa, $naziv, $napomena = '') {
         $log = LogOsoba::firstOrNew(['naziv' => $naziv, 'loggable_id' => $object->id]);
         $log->naziv = $naziv;
         $log->napomena = $napomena;
