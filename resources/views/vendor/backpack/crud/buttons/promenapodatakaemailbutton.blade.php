324482b5697b17d62d249a10868fde3f1bf04f82
bojan.maravic <bojan.maravic@ingkomora.rs>
2021-09-27 12:26:20 +0200

    promenapodatakaemailbutton.blade da se pojavi i za problematicne

--- resources/views/vendor/backpack/crud/buttons/promenapodatakaemailbutton.blade.php
+++ resources/views/vendor/backpack/crud/buttons/promenapodatakaemailbutton.blade.php
@@ -5,7 +5,7 @@
             class="btn btn-sm btn-link">
             <i class="la la-pencil"></i> Obradi
         </a>
-    @elseif($entry->obradjen === backpack_user()->id + 100)
+    @elseif($entry->obradjen === backpack_user()->id + 100 OR $entry->obradjen === backpack_user()->id + 200)
         <a
             href="{{ url($crud->route.'/'.$entry->getKey().'/promenapodatakaemail') }}"
             class="btn btn-sm btn-link">
