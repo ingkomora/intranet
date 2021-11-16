c355e3d8d05638f248753f63182bd116d24814f3
milan.simic <milan.simic@ingkomora.rs>
2021-11-16 15:52:08 +0100

    OsobaEditCrudController -> platili clanarinu filter 2017

--- app/Http/Controllers/Admin/OsobaEditCrudController.php
+++ app/Http/Controllers/Admin/OsobaEditCrudController.php
@@ -278,8 +278,9 @@ class OsobaEditCrudController extends CrudController
             'type' => 'dropdown',
             'label' => 'Plaćena članarina za godinu:'
         ], [
-            'Platili 2017' => '2017',
-            'Platili 2018' => '2018',
+            'Platio 2017' => '2017',
+            'Platio 2018' => '2018',
+            'Platio 2019' => '2019',
         ],
             function ($value) { // if the filter is active
                 $this->crud->addClause('whereHas', 'requests', function ($q) use ($value) {
@@ -333,35 +334,15 @@ class OsobaEditCrudController extends CrudController
 
     public function update()
     {
-        // do something before validation, before save, before everything; for example:
-        // $this->crud->addField(['type' => 'hidden', 'name' => 'author_id']);
-        // $this->crud->removeField('password_confirmation');
-
-        // Note: By default Backpack ONLY saves the inputs that were added on page using Backpack fields.
-        // This is done by stripping the request of all inputs that do NOT match Backpack fields for this
-        // particular operation. This is an added security layer, to protect your database from malicious
-        // users who could theoretically add inputs using DeveloperTools or JavaScript. If you're not properly
-        // using $guarded or $fillable on your model, malicious inputs could get you into trouble.
-
-        // However, if you know you have proper $guarded or $fillable on your model, and you want to manipulate
-        // the request directly to add or remove request parameters, you can also do that.
-        // We have a config value you can set, either inside your operation in `config/backpack/crud.php` if
-        // you want it to apply to all CRUDs, or inside a particular CrudController:
-        // $this->crud->setOperationSetting('saveAllInputsExcept', ['_token', '_method', 'http_referrer', 'current_tab', 'save_action']);
-        // The above will make Backpack store all inputs EXCEPT for the ones it uses for various features.
-        // So you can manipulate the request and add any request variable you'd like.
-        // $this->crud->getRequest()->request->add(['author_id'=> backpack_user()->id]);
-        // $this->crud->getRequest()->request->remove('password_confirmation');
-        // $this->crud->getRequest()->request->add(['author_id'=> backpack_user()->id]);
-        // $this->crud->getRequest()->request->remove('password_confirmation');
 //        todo: sta ako ima vise rekorda???
 //        dd($this->crud->getRequest()->status_id);
         $request = Request::where('osoba_id', $this->crud->getRequest()->id)
             ->where('request_category_id', 2)
             ->first();
-        if ($this->crud->getRequest()->status_id == PROBLEM) {
-            $request->status_id = PROBLEM;
-        } else {
+
+//        dd($request);
+        $request->status_id = $this->crud->getRequest()->status_id;
+        if ($this->crud->getRequest()->status_id == KREIRAN) {
             $request->status_id = OBRADJEN;
         }
         $request->save();
