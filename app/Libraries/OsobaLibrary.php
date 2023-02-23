96ac7f2c0f8125f03a3e04c1ea4534531f70fa04
milan.simic <milan.simic@ingkomora.rs>
2023-02-23 12:10:13 +0100

    feat: Libraries update

--- app/Libraries/OsobaLibrary.php
+++ app/Libraries/OsobaLibrary.php
@@ -11,23 +11,42 @@ use Illuminate\Database\Eloquent\Model;
 abstract class OsobaLibrary
 {
 
-    public static function osobaExists(string $jmbg): bool
+
+    /**
+     * @param string $jmbg
+     * @return Osoba|null
+     */
+    public static function get(string $jmbg): ?Osoba
     {
-        $osoba = Osoba::find($jmbg);
-        if (is_null($osoba))
-            return FALSE;
-
-        return TRUE;
-
+        return Osoba::find($jmbg);
     }
 
-    public static function getOsoba(string $jmbg)
-    {
-        $osoba = Osoba::find($jmbg);
-        if (!is_null($osoba))
-            return $osoba;
 
-        return null;
+    /**
+     * @param string $jmbg
+     * @return bool
+     */
+    public static function exists(string $jmbg): bool
+    {
+        return !is_null(self::get($jmbg));
+    }
+
+    /**
+     * @param string $jmbg
+     * @return bool
+     * Method returns bool for licence existence.
+     * If osoba doesnt exist it returns FALSE.
+     */
+    public static function hasLicence(string $jmbg): bool
+    {
+        $osoba = self::get($jmbg);
+
+        if ($osoba)
+            // if osoba exists return true|false from model relationship
+            return $osoba->licence->whereIn('status', ['A', 'N'])->isNotEmpty();
+        else
+            // if osoba doesn't exists
+            return FALSE;
     }
 
 
@@ -44,7 +63,7 @@ abstract class OsobaLibrary
      * @param Model $model
      * @return Sekcija
      */
-    public static function getZvanjeGrupa(Model $model): Sekcija
+    public static function getSekcija(Model $model): Sekcija
     {
         if (isset($model->osoba_id))
             $zg = $model->osoba->zvanjeId->sekcija;
